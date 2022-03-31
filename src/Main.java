cd7c8cba1fe3b400369b8c9e79345c277aab5df9
goofyiscool <hathahson2706@gmail.com>
2022-03-31 19:42:27 +0200

    Revert "Update"
    This reverts commit c8dc3995645e862e5ccda201843a22b0ec5a388e.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Scanner;
 
@@ -9,7 +8,6 @@ public class Main {
 
     public static Scanner sc = new Scanner(System.in);
     public static LinkedList<Ship> ships = new LinkedList<>();
-//    private HashMap<Ship, Container[]> containerShips = new HashMap<>();
     public static LinkedList<Container> containersShip = new LinkedList<>();
     public static LinkedList<Container> containersWarehouse = new LinkedList<>();
     public static LinkedList<Sender> senders = new LinkedList<>();
@@ -274,7 +272,7 @@ public class Main {
                         ships.add(ship);
                     case "sender":
                         Sender sender = new Sender(tmp[0], tmp[1], tmp[2], tmp[3]);
-
+                        
                         senders.add(sender);
                     case "containersShip", "containersWarehouse":
                         Container container = null;
